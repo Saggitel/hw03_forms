1221ddf23a83e84bd2764fed1d36d0e8035f03c0
Saggitel <lazba.timur@gmail.com>
2022-08-02 16:09:35 +0300

    Спринт 4 Ошибка 1

---
+++ yatube/posts/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+
+from .models import Post
+
+
+class PostForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['group'].empty_label = 'Группа не выбрана'
+
+    class Meta:
+        model = Post
+        fields = ('text', 'group',)
